3f9aff6958a19e48844e7de4fb889685d08b80ca
AndrewZabavnikow <72730015+AndrewZabavnikow@users.noreply.github.com>
2021-09-30 12:38:56 +0500

    Add files via upload

---
+++ OCHEN_WIDELAYUSHEESYA_NAZWANIE/AddWin.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace OCHEN_WIDELAYUSHEESYA_NAZWANIE
+{
+    /// <summary>
+    /// Логика взаимодействия для AddWin.xaml
+    /// </summary>
+    public partial class AddWin : Window
+    {
+        public AddWin()
+        {
+            InitializeComponent();
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ADDCLASS.DB.Люди.Add(new Люди 
+            {
+                Имя = One.Text,
+                Фамилия = Two.Text,
+            });
+            ADDCLASS.DB.SaveChanges();
+            MainWindow win = new MainWindow();
+            win.Show();
+            Close();
+        }
+    }
+}
---
+++ OCHEN_WIDELAYUSHEESYA_NAZWANIE/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OCHEN_WIDELAYUSHEESYA_NAZWANIE
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            AndrwZapEntities AZ = new AndrwZapEntities();
+            DeGas.ItemsSource = AZ.Люди.ToList();
+        }
+
+        private void Frst_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Scnd_Click()
+        {
+
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            AddWin win = new AddWin();
+            win.Show();
+        }
+    }
+}
